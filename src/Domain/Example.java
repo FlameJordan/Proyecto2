b62667aaa7ac3332deaff433c24d7b55ba4466ac
Jordan <carangatasonic@gmail.com>
2019-05-19 21:10:47 -0600

    Codigo para el boton reversa

--- src/Domain/Example.java
+++ src/Domain/Example.java
@@ -5,6 +5,8 @@
  */
 package Domain;
 
+import GUI.MainWindow;
+import com.sun.org.apache.bcel.internal.generic.AALOAD;
 import java.awt.Image;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -26,6 +28,13 @@ public class Example extends Character {
     private ImageIO imageIO;
     private ArrayList<Image> sprite;
     private boolean curve = false;
+    private ArrayList reverseRutePositionX = new ArrayList();
+    private ArrayList reverseRutePositionY = new ArrayList();
+    private ArrayList spriteReverseRute = new ArrayList();
+    private int counter = 0;
+    int countSprite = 0;
+    int countSpriteR = 10;
+    private int run=1;
 
     public Example(int x, int y, int imgNum) throws FileNotFoundException, IOException {
         super(x, y, imgNum);
@@ -51,13 +60,36 @@ public class Example extends Character {
     @Override
     public void run() {
         ArrayList<Image> sprite = super.getSprite();
-        while (true) {
+        while (run==1) {
             try {
-                
+
                 super.setX(x);
                 super.setY(y);
-                rute1(20, 1);
+                this.counter = this.spriteReverseRute.size() - 1;
+               
+                if (MainWindow.getInstance().isReverseRute() == false) {
+                    rute1(20, 1);
+                } else {
 
+                    for (int i = (reverseRutePositionX.size() + reverseRutePositionY.size()) - 1; i >= 0; i--) {
+                        Thread.sleep(38);
+                        super.setX((int) reverseRutePositionX.get(i / 2));
+                        super.setY((int) reverseRutePositionY.get(i / 2));
+                        if (MainWindow.getInstance().isReverseRute() == false) {
+                            break;
+                        }
+                        
+//                        super.setImage(sprite.get((int) spriteReverseRute.get(counter)));
+//                        if (counter > 0) {
+//                            counter--;
+//                        } else {
+//                            counter = spriteReverseRute.size() - 1;
+//
+//                        }
+                    }
+                    
+                }
+               
             } catch (BootstrapMethodError | InternalError br) {
             } catch (InterruptedException ex) {
                 Logger.getLogger(Example.class.getName()).log(Level.SEVERE, null, ex);
@@ -69,85 +101,99 @@ public class Example extends Character {
     }
 
     public void rute1(int repeat, int dir) throws InterruptedException, Throwable {
-        int countSprite = 0;
-        int countSpriteR = 10;
+
         if (dir == 1) {
-            for (int i = 0; i <= repeat; i++) {
-                Thread.sleep(60);
-                if (reverse == false) {
-                    if (countSprite >= 10) {
-                        countSprite = 0;
-                    }
-                    super.setImage(sprite.get(countSprite));
-                } else {
-                    if (countSpriteR >= 19) {
-                        countSpriteR = 10;
-                    }
-                    super.setImage(sprite.get(countSpriteR));
-                }
-                if (x < 560 && y > 160 && x >= 190 && curve == false) {
-                    x += 5;
-                    super.setX(x);
-                }
 
-                if (x >= 550 && y <= 266 && y >= 210 && curve == false) {
-                    x += 6;
-                    y -= 5;
-                    super.setX(x);
-                    super.setY(y);
+            Thread.sleep(60);
+            if (this.reverse == false) {
+                if (this.countSprite >= 10) {
+                    this.countSprite = 0;
                 }
-                if (y <= 228 && x >= 550 && y >= 163 && curve == false) {
-                    x -= 4;
-                    y -= 5;
-
-                    reverse = true;
-                    super.setX(x);
-                    super.setY(y);
+                this.spriteReverseRute.add(this.countSprite);
+                super.setImage(sprite.get(this.countSprite));
+            } else {
+                if (this.countSpriteR >= 19) {
+                    this.countSpriteR = 10;
                 }
-                if (y <= 162 && x >= 190 && curve == false) {
-                    if (x >= 550) {
-                        y -= 3;
-                    }
-                    x -= 6;
-                    super.setX(x);
-                    super.setY(y);
-                }
-
-                if (x <= 190 && x >= 145 && y <= 200) {
-                    if (y <= 225 && x <= 215) {
-                        curve = true;
-                        y += 4;
-                        x -= 4;
-                        super.setY(y);
-                        super.setX(x);
-                    }
-                }
-
-                if (x >= 135 && x <= 210 && y >= 195) {
-                    y += 3;
-                    x += 4;
-                    super.setY(y);
-                    super.setX(x);
-                    reverse = false;
-                } else if (x >= 210 && curve) {
-                    x += 5;
-
-                    super.setX(x);
-                }
-
-                if (countSprite >= 9) {
-                    countSprite = 0;
-                } else if(reverse==false) {
-                    countSprite++;
-                }
-
-                if (countSpriteR >= 19 ) {
-                    countSpriteR = 10;
-                } else if(reverse){
-                    countSpriteR++;
-                }
-
+                this.spriteReverseRute.add(this.countSpriteR);
+                super.setImage(this.sprite.get(this.countSpriteR));
             }
+            if (x < 560 && y > 160 && x >= 190 && curve == false) {
+                x += 5;
+                this.reverseRutePositionX.add(x);
+                this.reverseRutePositionY.add(y);
+                super.setX(x);
+                super.setY(y);
+            }
+
+            if (x >= 550 && y <= 266 && y >= 210 && curve == false) {
+                x += 6;
+                y -= 5;
+                reverseRutePositionX.add(x);
+                reverseRutePositionY.add(y);
+                super.setX(x);
+                super.setY(y);
+            }
+            if (y <= 228 && x >= 550 && y >= 163 && curve == false) {
+                x -= 4;
+                y -= 5;
+
+                reverseRutePositionX.add(x);
+                reverseRutePositionY.add(y);
+                reverse = true;
+                super.setX(x);
+                super.setY(y);
+            }
+            if (y <= 162 && x >= 190 && curve == false) {
+                if (x >= 550) {
+                    y -= 3;
+                }
+                x -= 6;
+                reverseRutePositionX.add(x);
+                reverseRutePositionY.add(y);
+                super.setX(x);
+                super.setY(y);
+            }
+
+            if (x <= 190 && x >= 145 && y <= 200) {
+                if (y <= 225 && x <= 215) {
+                    curve = true;
+                    y += 4;
+                    x -= 4;
+                    reverseRutePositionX.add(x);
+                    reverseRutePositionY.add(y);
+                    super.setY(y);
+                    super.setX(x);
+                }
+            }
+
+            if (x >= 135 && x <= 210 && y >= 195) {
+                y += 3;
+                x += 4;
+                reverseRutePositionX.add(x);
+                reverseRutePositionY.add(y);
+                super.setY(y);
+                super.setX(x);
+                reverse = false;
+            } else if (x >= 210 && curve) {
+                x += 5;
+                reverseRutePositionX.add(x);
+                reverseRutePositionY.add(y);
+                super.setX(x);
+            }
+
+            if (countSprite >= 9) {
+                countSprite = 0;
+            } else if (reverse == false) {
+                countSprite++;
+            }
+
+            if (countSpriteR >= 19) {
+                countSpriteR = 10;
+            } else if (reverse) {
+                countSpriteR++;
+            }
+
         }
     }
 
